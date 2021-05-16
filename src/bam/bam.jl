d370ebe28c9cd6f95704440c9bcc3ce527b7b6e5
Ciarán O’Mara <Ciaran.OMara@utas.edu.au>
2021-05-16 12:20:55 +1000

    Adjust BGZFStreams.virtualoffset
    https://github.com/BioJulia/BGZFStreams.jl/issues/22#issuecomment-839746122

--- src/bam/bam.jl
+++ src/bam/bam.jl
@@ -15,6 +15,20 @@ import BioGenerics: isfilled, header
 
 import GenomicFeatures: eachoverlap
 
+# Overwrite virtualoffset method as per https://github.com/BioJulia/BGZFStreams.jl/issues/22#issuecomment-839746122.
+function BGZFStreams.virtualoffset(stream::BGZFStreams.BGZFStream)
+    if stream.mode == BGZFStreams.READ_MODE
+        i = BGZFStreams.ensure_buffered_data(stream)
+        if i == 0
+            block = stream.blocks[end]
+        else
+            block = stream.blocks[i]
+        end
+    else
+        block = stream.blocks[1]
+    end
+    return BGZFStreams.VirtualOffset(block.block_offset, block.position - 1)
+end
 
 include("bai.jl")
 include("auxdata.jl")
